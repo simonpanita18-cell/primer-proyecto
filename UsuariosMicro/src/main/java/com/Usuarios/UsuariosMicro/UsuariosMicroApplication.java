ce1e68e154d74612bc3c7fe9c1a08047e3dab6ee
simone-userr <si.hercules@duocuc.cl>
2026-04-18 22:45:51 -0400

    primer commit

---
+++ UsuariosMicro/src/main/java/com/Usuarios/UsuariosMicro/UsuariosMicroApplication.java
@@ -0,0 +1,13 @@
+package com.Usuarios.UsuariosMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosMicroApplication.class, args);
+	}
+
+}
---
+++ UsuariosMicro/src/test/java/com/Usuarios/UsuariosMicro/UsuariosMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Usuarios.UsuariosMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
