e556bdc9f8861534bd9959f41d4c580e6f989bcc
simon <simonpanita18@gmail.com>
2026-05-23 00:49:54 -0400

    Prestamo

---
+++ src/test/java/com/bibliogo/prestamo/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliogo.prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
