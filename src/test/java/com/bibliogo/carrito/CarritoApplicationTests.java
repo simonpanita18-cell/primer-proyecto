ea049f13564aea8e55d0f981fc61352913e42b16
simon <simonpanita18@gmail.com>
2026-05-23 19:18:26 -0400

    prestamo

---
+++ src/test/java/com/bibliogo/carrito/CarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliogo.carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
