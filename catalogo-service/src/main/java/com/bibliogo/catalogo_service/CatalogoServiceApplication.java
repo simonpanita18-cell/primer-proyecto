eb9e14ab38689fb974fb53743312b45956c542ea
simon <simonpanita18@gmail.com>
2026-05-22 23:48:31 -0400

    catalogo service

---
+++ catalogo-service/src/main/java/com/bibliogo/catalogo_service/CatalogoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bibliogo.catalogo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoServiceApplication.class, args);
+	}
+
+}
