acc6a8dda5d681e6b2980e24458b01d54635a58d
simon <simonpanita18@gmail.com>
2026-05-24 00:28:29 -0400

    a

---
+++ resena/src/main/java/com/bibliogo/resena/ResenaApplication.java
@@ -0,0 +1,13 @@
+package com.bibliogo.resena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenaApplication.class, args);
+	}
+
+}
---
+++ resena/src/test/java/com/bibliogo/resena/ResenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliogo.resena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
