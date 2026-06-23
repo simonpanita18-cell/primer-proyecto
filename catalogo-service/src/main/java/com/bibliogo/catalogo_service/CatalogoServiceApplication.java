31bcba4f388c33fd65f273907949c6b5af331ab0
simon <simonpanita18@gmail.com>
2026-06-23 00:50:19 -0400

    comentario

--- catalogo-service/src/main/java/com/bibliogo/catalogo_service/CatalogoServiceApplication.java
+++ catalogo-service/src/main/java/com/bibliogo/catalogo_service/CatalogoServiceApplication.java
@@ -8,6 +8,10 @@ public class CatalogoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoServiceApplication.class, args);
+		System.out.println("================================================");
+		System.out.println(" Catalogo Service corriendo en: http://localhost:8082");
+		System.out.println(" Swagger UI corriendo en: http://localhost:8082/swagger-ui/index.html");
+		System.out.println("================================================");
 	}
 
 }
